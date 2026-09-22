04b0095c477dc5137271d3a3269385f36578493e
IgnacioZabala <82279524+IgnacioZabala@users.noreply.github.com>
2026-09-22 10:43:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -141,7 +141,7 @@ def generar_pdf_base(titulo: str, subtitulo: str, metricas: list, headers: list,
   pdf.add_page()
   
   if os.path.exists("logo.png"):
-    pdf.image("logo.png", x=82, y=8, w=45)
+    pdf.image("logo.png", x=82, y=8, w=55)
     pdf.set_y(50)
   else:
     pdf.set_y(20)
